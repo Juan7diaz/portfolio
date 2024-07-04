3dbc6909ce8114cfb2597b0afc91dd38306ea477
juan7diaz <juanluisdiazguerrero@gmail.com>
2024-07-03 23:08:25 -0500

    feat: add skeleton carousel

--- src/components/common/Carousel.tsx
+++ src/components/common/Carousel.tsx
@@ -9,56 +9,67 @@ function Carousel({ imgs = [] }: { imgs: string[] }) {
     imgs.length > 0 ? imgs[0] : 'no image',
   );
 
+  const [isLoading, setIsLoading] = useState<boolean[]>(
+    Array(imgs.length).fill(true),
+  );
+
   if (imgs.length === 0) return <div />;
 
-
-
   const nextImg = () => {
-
     const currentIndex = imgs.indexOf(currentImg);
-
     const newIndex = currentIndex + 1 === imgs.length ? 0 : currentIndex + 1;
-
     setCurrentImg(imgs[newIndex]);
   };
 
   const prevImg = () => {
     const currentIndex = imgs.indexOf(currentImg);
-
     const newIndex =
       currentIndex - 1 === -1 ? imgs.length - 1 : currentIndex - 1;
 
     setCurrentImg(imgs[newIndex]);
   };
 
+  const changeLoadingByIndex = (index: number, arrBooleans: boolean[]) => {
+    const newIsLoading = [...arrBooleans];
+    newIsLoading[index] = false;
+    setIsLoading(newIsLoading);
+  };
+
   return (
     <div className="flex flex-col items-center pb-10">
-      <div className="flex flex-row items-center justify-center pb-2">
+      <div className="relative flex flex-row items-center justify-center pb-2">
         <button
           type="button"
-          className="text-2xl text-text-primary hover:bg-slate-50/15"
+          className="hidden text-2xl text-text-primary hover:bg-slate-50/15 md:block"
           onClick={prevImg}
-          aria-label="botón para mostrar la siguiente imagen"
+          aria-label="botón para mostrar la imagen anterior"
         >
           <IoIosArrowBack className="h-10 w-10 text-primary-base" />
         </button>
 
-        {imgs.map((img) => (
-          <Image
-            key={img}
-            src={img}
-            alt="Project image h-500 w-500"
-            width={500}
-            height={500}
-            className={`rounded-md transition-all duration-300 ease-in-out ${
-              img === currentImg ? 'block' : 'hidden'
-            }`}
-          />
-        ))}
+        <div className="h-500 w-375 relative">
+          {isLoading[imgs.indexOf(currentImg)] && (
+            <div className="h-500 w-375 absolute inset-0 animate-pulse rounded-md bg-gray-300" />
+          )}
+          {imgs.map((img, i) => (
+            <Image
+              key={img}
+              src={img}
+              alt="Project image h-500 w-375"
+              width={500}
+              height={375}
+              onLoad={() => changeLoadingByIndex(i, isLoading)}
+              priority={i === 0}
+              className={`rounded-md transition-all duration-300 ease-in-out ${
+                img === currentImg ? 'block' : 'hidden'
+              }`}
+            />
+          ))}
+        </div>
 
         <button
           type="button"
-          className="text-2xl text-text-primary hover:bg-slate-50/15"
+          className="hidden text-2xl text-text-primary hover:bg-slate-50/15 md:block"
           onClick={nextImg}
           aria-label="botón para mostrar la siguiente imagen"
         >
@@ -66,18 +77,36 @@ function Carousel({ imgs = [] }: { imgs: string[] }) {
         </button>
       </div>
 
-      <div className="flex flex-row items-center justify-center">
+      <div className="mt-4 flex flex-row items-center justify-center">
+        <button
+          type="button"
+          className="mx-2 text-2xl text-text-primary hover:bg-slate-50/15 md:hidden"
+          onClick={prevImg}
+          aria-label="botón para mostrar la imagen anterior"
+        >
+          <IoIosArrowBack className="h-8 w-8 text-primary-base" />
+        </button>
+
         {imgs.map((img, index) => (
           <button
             key={img}
             type="button"
-            className={`mx-2 h-2 w-2 rounded-full ${
+            className={`mx-2 hidden h-2 w-2 rounded-full md:block ${
               img === currentImg ? 'bg-primary-base' : 'bg-text-tertiary'
             }`}
             onClick={() => setCurrentImg(img)}
             aria-label={`botón para mostrar la imagen ${index + 1}`}
           />
         ))}
+
+        <button
+          type="button"
+          className="mx-2 text-2xl text-text-primary hover:bg-slate-50/15 md:hidden"
+          onClick={nextImg}
+          aria-label="botón para mostrar la siguiente imagen"
+        >
+          <IoIosArrowForward className="h-8 w-8 text-primary-base" />
+        </button>
       </div>
     </div>
   );
